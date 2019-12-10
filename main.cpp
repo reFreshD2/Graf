77267e654ffe70802bb1c599205c0be0aadfff1c
refreshd2 <refreshd2@mail.ru>
2019-12-10 14:37:54 +1000

    Ostrouhova's tests

--- main.cpp
+++ main.cpp
@@ -16,35 +16,8 @@ struct adj {
 class Graf {
     V *H;
 
-    bool isInV(V *Head, int name) {
-        bool isIn = false;
-        if (Head != nullptr) {
-            while (Head != nullptr) {
-                if (Head->name == name) {
-                    isIn = true;
-                    return isIn;
-                }
-                Head = Head->next;
-            }
-        }
-        return isIn;
-    }
-
-    bool isInAdj(adj *Head, int name) {
-        bool isIn = false;
-        if (Head != nullptr) {
-            while (Head != nullptr) {
-                if (Head->name->name == name) {
-                    isIn = true;
-                    return isIn;
-                }
-                Head = Head->next;
-            }
-        }
-        return isIn;
-    }
-
-    V *searchV(V *Head, int name) {
+    V *searchVPointer(int name) {
+        V *Head = H;
         if (Head != nullptr) {
             while (Head != nullptr) {
                 if (Head->name == name) {
@@ -76,7 +49,7 @@ public:
     }
 
     int addV(int name) {
-        if (!isInV(H, name)) {
+        if (!searchV(name)) {
             V *temp = new V;
             temp->next = nullptr;
             temp->name = name;
@@ -108,12 +81,12 @@ public:
     }
 
     int addE(int v1, int v2) {
-        if (isInV(H, v1) && isInV(H, v2)) {
-            V *temp1 = searchV(H, v1);
-            V *temp2 = searchV(H, v2);
-            if (!isInAdj(temp1->E, v2)) {
+        if (searchV(v1) && searchV(v2)) {
+            V *temp1 = searchVPointer(v1);
+            V *temp2 = searchVPointer(v2);
+            if (!searchE(v1, v2)) {
                 adj *newV1 = new adj;
-                newV1->name = searchV(H, v2);
+                newV1->name = searchVPointer(v2);
                 newV1->next = nullptr;
                 if (temp1->E != nullptr) {
                     adj *temp3 = temp1->E;
@@ -137,9 +110,9 @@ public:
             } else {
                 return 2; // уже есть ребро (v1,v2)
             }
-            if (!isInAdj(temp2->E, v1)) {
+            if (!searchE(v2, v1)) {
                 adj *newV1 = new adj;
-                newV1->name = searchV(H, v1);
+                newV1->name = searchVPointer(v1);
                 newV1->next = nullptr;
                 if (temp2->E != nullptr) {
                     adj *temp3 = temp2->E;
@@ -170,10 +143,10 @@ public:
     }
 
     int delE(int v1, int v2) {
-        if (isInV(H, v1) && isInV(H, v2)) {
-            V *temp1 = searchV(H, v1);
-            V *temp2 = searchV(H, v2);
-            if (isInAdj(temp1->E, v2) && isInAdj(temp2->E, v1)) {
+        if (searchV(v1) && searchV(v2)) {
+            V *temp1 = searchVPointer(v1);
+            V *temp2 = searchVPointer(v2);
+            if (searchE(v1, v2) && searchE(v2, v1)) {
                 adj *adj1 = temp1->E;
                 adj *adj2 = temp2->E;
                 if (adj1->name->name == v2) {
@@ -206,7 +179,7 @@ public:
     }
 
     int delV(int v1) {
-        if (isInV(H, v1)) {
+        if (searchV(v1)) {
             V *temp = H;
             while (temp != nullptr) {
                 delE(v1, temp->name);
@@ -247,57 +220,59 @@ public:
             cout << "----------------------------------" << endl;
         }
     }
+
+    bool searchV(int name) {
+        bool isIn = false;
+        V* Head = H;
+        if (Head != nullptr) {
+            while (Head != nullptr) {
+                if (Head->name == name) {
+                    isIn = true;
+                    return isIn;
+                }
+                Head = Head->next;
+            }
+        }
+        return isIn;
+    }
+
+    bool searchE(int v1, int v2) {
+        bool isIn = false;
+        adj *Head = searchVPointer(v1)->E;
+        if (Head != nullptr) {
+            while (Head != nullptr) {
+                if (Head->name->name == v2) {
+                    isIn = true;
+                    return isIn;
+                }
+                Head = Head->next;
+            }
+        }
+        return isIn;
+    }
 };
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    Graf graf1;
-    graf1.addV(2) == 1; // добавление в пустой
-    graf1.print();
-    graf1.addV(4) == 1; // добавление в конец
-    graf1.print();
-    graf1.addV(1) == 1;// добавление в начало
-    graf1.print();
-    graf1.addV(3) == 1;// добавление в середину
-    graf1.print();
-    if (graf1.addV(2) == 1) {
-        cout << "Duplicate nodes" << endl;
-    }; // добавление повтора
-    if (graf1.addE(1, 5) == 1) {
-        cout << "Haven't nodes v1 or v2" << endl;
-    }; // нет вершины v2
-    if (graf1.addE(5, 1) == 1) {
-        cout << "Haven't nodes v1 or v2" << endl;
-    };// нет вершины v1
-    if (graf1.addE(5, 5) == 1) {
-        cout << "Haven't nodes v1 or v2" << endl;
-    }; // нет вершин v1 и v2
-    graf1.addE(1, 2); // добавление в пустой
-    graf1.print();
-    graf1.addE(1, 4); // добавление в конец
-    graf1.print();
-    graf1.addE(1, 1); // добавление в начало
-    graf1.print();
-    graf1.addE(1, 3); // добавление в середину
-    graf1.print();
-    if (graf1.addE(1, 2) == 2) {
-        cout << "The edge (v1,v2) is already in the graph" << endl;
-    } // добавление повторяющегося ребра
-    graf1.delE(1, 4); // удаление с начала и в середине
-    graf1.print();
-    if (graf1.delE(1, 5) == 1) { // удаление ребра с несуществующей вершиной
-        cout << "Haven't nodes v1 or v2" << endl;
+    Graf g;
+
+    for (int i=1;i<10;i++) {
+        g.addV(i);
+        g.addV(10-i);
     }
-    if (graf1.delE(5, 1) == 1) {
-        cout << "Haven't nodes v1 or v2" << endl;
+    for (int i=1;i<10;i++) {
+        for (int j = 1; j < 10; j++) {
+g.addE(i,j);
+        }
     }
-    if (graf1.delE(1, 4) == 2) { // удаление не существующего ребра
-        cout << "No edges (v1,v2) in the graph" << endl;
-    }
-    graf1.delV(3); // удаление существующей вершины
-    graf1.print();
-    if (graf1.delV(3) == 1) {
-        cout << "Haven't node v1" << endl;
-    }
+
+cout<<endl;
+    g.print();
+
+    for(int i=1;i<10;i=i+2)
+        g.delE(i,i);
+
+    cout<<endl;
+    g.print();
     return 0;
 }
